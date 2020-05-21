ed1ec9ca45c2fe53d18035e910a190550e5a1d5c
Tatsunomiya <Black1940G>
2020-05-21 18:37:59 +0900

    testeststes

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) {(granted, error) in
+        
+        
     }
+    center.delegate = self
+    
+    return true
+}
 
     // MARK: UISceneSession Lifecycle
 
@@ -30,6 +38,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletetionHandler completionHandler: @escaping(UNNotificationPresentationOptions) ->Void) {
+        
+        completionHandler([.alert, .sound])
     }
 
 
