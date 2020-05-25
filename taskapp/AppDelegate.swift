c347bc3cb3c4154caeb30c16a3aeddc80463d17a
Tatsunomiya <Black1940G>
2020-05-25 21:50:26 +0900

    ってえて

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -46,7 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     }
     
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletetionHandler completionHandler: @escaping(UNNotificationPresentationOptions) ->Void) {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping(UNNotificationPresentationOptions) ->Void) {
         
         completionHandler([.alert, .sound])
     }
